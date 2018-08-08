1c01d6742e623dc420601a56cdcfd73fb213e22d
marutyan76 <city.mati76@gmail.com>
2018-08-08 16:19:57 +0900

    update ver01

---
+++ script/pa_install.sh
@@ -0,0 +1,12 @@
+yum groupinstall "Development Tools"
+yum install httpd24 httpd24-devel git openssl-devel readline-devel libcurl-devel
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:${PATH}"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+rbenv install -v 2.5.0
+rbenv rehash
+rbenv global 2.5.0
+gem install passenger --no-rdoc --no-ri
+passenger-install-apache2-module
